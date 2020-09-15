09205bb88aaaa9ebf129e35393caa6c0772004ca
Ardient <ardien@126.com>
2020-09-15 22:58:27 +0800

    Develop Netty WebSocket

---
+++ src/main/java/websocket/WebSocketServer.java
@@ -0,0 +1,50 @@
+package websocket;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+public class WebSocketServer {
+    public void run(int port) {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ch.pipeline()
+                                    .addLast("http-codec",new HttpServerCodec())
+                                    .addLast("aggregator", new HttpObjectAggregator(65536))
+                                    .addLast("http-chunked", new ChunkedWriteHandler())
+                                    .addLast(new WebSocketServerHandler());
+
+                        }
+                    });
+
+            ChannelFuture future = server.bind(port).sync();
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        WebSocketServer server = new WebSocketServer();
+        server.run(8090);
+    }
+}
---
+++ src/main/java/websocket/WebSocketServerHandler.java
@@ -0,0 +1,84 @@
+package websocket;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.codec.http.websocketx.*;
+import io.netty.util.CharsetUtil;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+import java.util.Date;
+
+import static io.netty.handler.codec.http.HttpUtil.setContentLength;
+
+public class WebSocketServerHandler extends SimpleChannelInboundHandler<Object> {
+
+    private WebSocketServerHandshaker handshaker;
+    protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
+        // 如果是传统HTTP请求，返回Status Code 400
+        if (msg instanceof FullHttpRequest) {
+            handleHttpRequest(ctx, (FullHttpRequest)msg);
+        }else if (msg instanceof WebSocketFrame){
+            handleWebSocketFrame(ctx, (WebSocketFrame)msg);
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+
+    private void handleWebSocketFrame(ChannelHandlerContext ctx, WebSocketFrame frame) {
+        if (frame instanceof CloseWebSocketFrame) {
+            handshaker.close(ctx.channel(), (CloseWebSocketFrame) frame.retain());
+            return;
+        }
+        //是否为Ping
+        if (frame instanceof PingWebSocketFrame) {
+            ctx.channel().write(new PongWebSocketFrame(frame.content().retain()));
+            return;
+        }
+
+        // 去除非法的二进制消息
+        if (!(frame instanceof TextWebSocketFrame)) {
+            throw new UnsupportedOperationException(String.format("%s frame type not supported", frame.getClass().getName()));
+        }
+
+        // 返回应答消息
+        String msg = ((TextWebSocketFrame) frame).text();
+        System.out.printf("%s receive %s" + ctx.channel(), msg);
+        ctx.channel().write(new TextWebSocketFrame(msg + ", 欢迎使用Netty. 时间：" + new Date().toString()));
+    }
+
+    private void handleHttpRequest(ChannelHandlerContext ctx, FullHttpRequest request) {
+        if (!request.decoderResult().isSuccess() || ("websocket".equals(request.headers().get("Upgrade")))) {
+            sendHttpResponse(ctx, request, new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_REQUEST));
+            return;
+        }
+        WebSocketServerHandshakerFactory factory = new WebSocketServerHandshakerFactory("ws://localhost:8090/websocket", null, false);
+        handshaker = factory.newHandshaker(request);
+        if (handshaker == null) {
+            WebSocketServerHandshakerFactory.sendUnsupportedVersionResponse(ctx.channel());
+        }else {
+            handshaker.handshake(ctx.channel(), request);
+        }
+    }
+
+    private void sendHttpResponse(ChannelHandlerContext ctx, FullHttpRequest request, FullHttpResponse response) {
+        if (response.status().code() != 200) {
+            ByteBuf buffer = Unpooled.copiedBuffer(response.status().toString(), CharsetUtil.UTF_8);
+            response.content().writeBytes(buffer);
+            buffer.release();
+            setContentLength(response, response.content().readableBytes());
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+    }
+}
